29d5fd591f965f506c3ceed495b9a677e162bf38
Arm <armsithuaung@gmail.com>
2022-08-17 02:14:54 +0700

    a

---
+++ meail.php
@@ -0,0 +1,21 @@
+<?php
+//get data from form  
+
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$number= $_POST['mobile'];
+
+$to = "do65770@mail.com";
+
+$subject = "Mail From MidtownThai";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message ."
+\r\n Mobile number =". $number;
+$headers = "From: noreply@midtownThai.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
